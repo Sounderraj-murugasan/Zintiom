5ffdbff07b9decd6befe98448f96c36167a4cf8f
Sounderraj-murugasan <sounderraj.murugasan@gmail.com>
2024-12-03 22:49:21 +0530

    bootstrap changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,46 @@
-function toggleNavbar() {
-  var nav = document.getElementById("nav_bar");
-  nav.classList.toggle("show_hide");
-}
-
-window.onresize = resizeFn;
-resizeFn();
-
-function resizeFn() {
-  var nav = document.getElementById("nav_bar");
-  if (nav !== null) {
-    if (window.innerWidth < 992) {
-      nav.classList.add("show_hide");
-    } else {
-      nav.classList.remove("show_hide");
-    }
-  }
-}
+$(document).ready(function () {
+  $(".js-slick-container").slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    spaceBetween: 30,
+    responsive: [
+      {
+        breakpoint: 1445,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (window.innerWidth < 992) {
-    var nav = document.getElementById("nav_bar");
-    if (nav !== null) {
-      nav.classList.add("show_hide");
-    }
+  const carouselContainer = document.querySelector(".js-slick-container");
+  const carouselItem = document.querySelector(".client-card");
+
+  const numberOfItems = 10;
+
+  for (let i = 0; i < numberOfItems; i++) {
+    const clonedItem = carouselItem.cloneNode(true);
+    carouselContainer.appendChild(clonedItem);
   }
 });
